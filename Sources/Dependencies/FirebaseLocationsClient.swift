aa17c7b355367a31dc017768be34fa6cd5b5c71f
Roman Podymov <podymfrombryansk@yandex.com>
2025-08-04 10:09:57 +0200

    Formatting

--- Sources/Dependencies/FirebaseLocationsClient.swift
+++ Sources/Dependencies/FirebaseLocationsClient.swift
@@ -6,64 +6,57 @@
 //  Copyright © 2025 BarBeeQ. All rights reserved.
 //
 
-import Combine
+import CombineFirebaseAuth
 import ComposableArchitecture
-@preconcurrency import FirebaseAuth
+import FirebaseAuth
 import FirebaseCore
 import FirebaseFirestore
 
 extension LocationsClient {
-    static let firebase = {
-        var listener: NSObjectProtocol!
-        return LocationsClient(setup: {
-            FirebaseApp.configure()
-        }, locations: {
-            let collection = Firestore.firestore().collection("BBQLocation")
-            return try await collection.getDocuments().documents.map {
-                let data = $0.data()
-                return BarBeeQLocation(
-                    name: (data["name"] as? String) ?? "",
-                    location: .init(
-                        latitude: (data["location"] as? GeoPoint)?.latitude ?? .zero,
-                        longitude: (data["location"] as? GeoPoint)?.longitude ?? .zero
-                    ),
-                    photo: {
-                        if let data = data["photo"] as? String {
-                            return Data(base64Encoded: data) ?? .init()
-                        }
-                        return .init()
-                    }()
-                )
-            }
-        }, addLocation: { location in
-            let collection = Firestore.firestore().collection("BBQLocation")
-            try await collection.addDocument(data: [
-                "name": location.name,
-                "location": GeoPoint(
-                    latitude: location.location.latitude,
-                    longitude: location.location.longitude
+    static let firebase = LocationsClient(setup: {
+        FirebaseApp.configure()
+    }, locations: {
+        let collection = Firestore.firestore().collection("BBQLocation")
+        return try await collection.getDocuments().documents.map {
+            let data = $0.data()
+            return BarBeeQLocation(
+                name: (data["name"] as? String) ?? "",
+                location: .init(
+                    latitude: (data["location"] as? GeoPoint)?.latitude ?? .zero,
+                    longitude: (data["location"] as? GeoPoint)?.longitude ?? .zero
                 ),
-                "photo": {
-                    let encodedImage = location.photo?.base64EncodedString() ?? ""
-                    return encodedImage
+                photo: {
+                    if let data = data["photo"] as? String {
+                        return Data(base64Encoded: data) ?? .init()
+                    }
+                    return .init()
                 }()
-            ])
-        }, signIn: { email, password in
-            try await Auth.auth().signIn(withEmail: email, password: password)
-        }, isSignedIn: {
-            let publisher = CurrentValueSubject<Bool, Never>(false)
-            Auth.auth().addStateDidChangeListener { _, user in
-                publisher.send(user != nil)
-            }
-            return publisher.eraseToAnyPublisher().values
-        }, signOut: {
-            try Auth.auth().signOut()
-        }, registerUser: { email, password in
-            try await Auth.auth().createUser(withEmail: email, password: password)
-        }, resetPassword: { email in
-            try await Auth.auth().sendPasswordReset(withEmail: email)
-        }, deleteAccount: {
-            Auth.auth().currentUser?.delete()
-        })
-    }()
+            )
+        }
+    }, addLocation: { location in
+        let collection = Firestore.firestore().collection("BBQLocation")
+        try await collection.addDocument(data: [
+            "name": location.name,
+            "location": GeoPoint(
+                latitude: location.location.latitude,
+                longitude: location.location.longitude
+            ),
+            "photo": {
+                let encodedImage = location.photo?.base64EncodedString() ?? ""
+                return encodedImage
+            }()
+        ])
+    }, signIn: { email, password in
+        try await Auth.auth().signIn(withEmail: email, password: password)
+    }, isSignedIn: {
+        Auth.auth().stateDidChangePublisher.map { $0 != nil }.values
+    }, signOut: {
+        try Auth.auth().signOut()
+    }, registerUser: { email, password in
+        try await Auth.auth().createUser(withEmail: email, password: password)
+    }, resetPassword: { email in
+        try await Auth.auth().sendPasswordReset(withEmail: email)
+    }, deleteAccount: {
+        Auth.auth().currentUser?.delete()
+    })
 }
